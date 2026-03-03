87c4e9a2441ca7286e2e0c6f59f9861278403576
AlainGZ <devalgz955@gmail.com>
2026-03-03 18:07:45 -0500

    feat: implement registrar salida with stock validation

--- domain/src/main/java/com/app/inventario/domain/service/MovimientoService.java
+++ domain/src/main/java/com/app/inventario/domain/service/MovimientoService.java
@@ -47,4 +47,38 @@ public class MovimientoService implements MovimientoUseCase {
 		return movimientoRepository.guardar(movimiento);
 	}
 
+	@Override
+	public Movimiento registrarSalida(Long productoId, Integer cantidad, String motivo){
+
+		productoRepository.buscarPorId(productoId)
+				.orElseThrow(() -> new IllegalArgumentException(
+						"No existe un producto con id: " + productoId
+				));
+
+		if (cantidad <= 0){
+			throw new IllegalArgumentException(
+					"La cantidad debe ser mayor a cero"
+			);
+		}
+
+		Integer stockActual = movimientoRepository.calcularStockActual(productoId);
+		if (cantidad > stockActual){
+			throw new IllegalArgumentException(
+					"Stock insuficiente. El stock actual es: " + stockActual + " unidades."
+			);
+		}
+
+		Movimiento movimiento = Movimiento.builder()
+				.tipo(TipoMovimiento.SALIDA)
+				.cantidad(cantidad)
+				.motivo(motivo)
+				.fecha(LocalDateTime.now())
+				.creadoEn(LocalDateTime.now())
+				.productoId(productoId)
+				.build();
+
+		return movimientoRepository.guardar(movimiento);
+
+	}
+
 }
